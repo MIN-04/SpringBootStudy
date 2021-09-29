2b8302eeacbd038e13ab6de96f9e3e2d114bb4ad
Mandy <mandy@plgrim.com>
2021-09-29 10:39:51 +0900

    21.09.29 repository test 전부 통과

--- src/test/java/spring/study/Member/domain/services/MemberRepositoryTest.java
+++ src/test/java/spring/study/Member/domain/services/MemberRepositoryTest.java
@@ -1,16 +1,18 @@
 package spring.study.Member.domain.services;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 import spring.study.Member.domain.aggregates.Member;
 import spring.study.Member.domain.valueObjects.MemberAddressInfo;
 import spring.study.Member.domain.valueObjects.MemberBasicInfo;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,16 +21,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //@DataJpaTest
 @SpringBootTest
+@Transactional
 class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    Member member;
+    Member member1;
+    Member member2;
+    Member member3;
 
     @BeforeEach
     void setUp() {
-
-        MemberBasicInfo memberBasicInfo = MemberBasicInfo.builder()
+        // member1
+        MemberBasicInfo memberBasicInfo1 = MemberBasicInfo.builder()
                 .password("abcd1!")
                 .name("홍길동")
                 .mobileNum("010-1111-1111")
@@ -36,14 +41,53 @@ class MemberRepositoryTest {
                 .birthday("001122")
                 .build();
 
-        MemberAddressInfo memberAddressInfo = MemberAddressInfo.builder()
+        MemberAddressInfo memberAddressInfo1 = MemberAddressInfo.builder()
                 .address("Seoul")
                 .build();
 
-        member = Member.builder()
+        member1 = Member.builder()
                 .email("hong@naver.com")
-                .memberBasicInfo(memberBasicInfo)
-                .memberAddressInfo(memberAddressInfo)
+                .memberBasicInfo(memberBasicInfo1)
+                .memberAddressInfo(memberAddressInfo1)
+                .build();
+
+
+        // member2
+        MemberBasicInfo memberBasicInfo2 = MemberBasicInfo.builder()
+                .password("abcd1!")
+                .name("박땡땡")
+                .mobileNum("010-1111-2222")
+                .gender("M")
+                .birthday("991203")
+                .build();
+
+        MemberAddressInfo memberAddressInfo2 = MemberAddressInfo.builder()
+                .address("Seoul")
+                .build();
+
+        member2 = Member.builder()
+                .email("park@naver.com")
+                .memberBasicInfo(memberBasicInfo2)
+                .memberAddressInfo(memberAddressInfo2)
+                .build();
+
+        // member3
+        MemberBasicInfo memberBasicInfo3 = MemberBasicInfo.builder()
+                .password("abcd1!")
+                .name("최영영")
+                .mobileNum("010-1111-3333")
+                .gender("F")
+                .birthday("010302")
+                .build();
+
+        MemberAddressInfo memberAddressInfo3 = MemberAddressInfo.builder()
+                .address("Seoul")
+                .build();
+
+        member3 = Member.builder()
+                .email("choi@naver.com")
+                .memberBasicInfo(memberBasicInfo3)
+                .memberAddressInfo(memberAddressInfo3)
                 .build();
     }
 
@@ -52,25 +96,25 @@ class MemberRepositoryTest {
     void save(){
         //given
         //when
-        Member result = memberRepository.save(member);
+        Member result = memberRepository.save(member1);
 
         //then
-        assertThat(result).usingRecursiveComparison().isEqualTo(member); //객체 안에 있는 객체 비교
+        assertThat(result).usingRecursiveComparison().isEqualTo(member1); //객체 안에 있는 객체 비교
     }
 
     @Test
     @DisplayName("이메일 또는 전화번호 중복 찾기")
     void findMemberByEmailOrMemberBasicInfo_MobileNum() {
         //given
-        memberRepository.save(member);
+        memberRepository.save(member1);
 
         //when
         //case 1. 이메일만 같을 때
         Optional<Member> result1 = memberRepository
-                .findMemberByEmailOrMemberBasicInfo_MobileNum("hong@naver.com", "010-1111-2222");
+                .findMemberByEmailOrMemberBasicInfo_MobileNum("hong@naver.com", "010-2222-1111");
         //case 2. 전화번호만 같을 때
         Optional<Member> result2 = memberRepository
-                .findMemberByEmailOrMemberBasicInfo_MobileNum("park@naver.com", "010-1111-1111");
+                .findMemberByEmailOrMemberBasicInfo_MobileNum("hongAAA@naver.com", "010-1111-1111");
         //case 3. 이메일과 전화번호 둘 다 같을 때
         Optional<Member> result3 = memberRepository
                 .findMemberByEmailOrMemberBasicInfo_MobileNum("hong@naver.com", "010-1111-1111");
@@ -78,13 +122,13 @@ class MemberRepositoryTest {
         //then
         //case 1
         assertFalse(result1.isEmpty()); //false여야 통과
-        assertThat(result1.get()).usingRecursiveComparison().isEqualTo(member);
+        assertThat(result1.get()).usingRecursiveComparison().isEqualTo(member1);
         //case 2
         assertFalse(result2.isEmpty());
-        assertThat(result2.get()).usingRecursiveComparison().isEqualTo(member);
+        assertThat(result2.get()).usingRecursiveComparison().isEqualTo(member1);
         //case 3
         assertFalse(result3.isEmpty());
-        assertThat(result3.get()).usingRecursiveComparison().isEqualTo(member);
+        assertThat(result3.get()).usingRecursiveComparison().isEqualTo(member1);
 
     }
 
@@ -92,21 +136,42 @@ class MemberRepositoryTest {
     @DisplayName("회원 목록 검색")
     void findAll() {
         //given
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+
         //when
+        int page = 1;
+        int pageCount = 2;
+        PageRequest pageRequest = PageRequest.of(page, pageCount);
+        Page<Member> members = memberRepository.findAll(pageRequest);
+
         //then
+        List<Member> contents = members.getContent();
+        assertFalse(contents.isEmpty());
+
+        int dataSize = members.getContent().size();
+        assertThat(dataSize <= pageCount).isTrue();
+
+//        assertThat(memberList).usingRecursiveComparison()(member, member2);
+        //https://tecoble.techcourse.co.kr/post/2020-11-03-assertJ_methods/
+        // containsExactly : 순서를 포함해서 정확히 일치
+        // assertThat(memberList).containsExactly(member, member2);
+        // assertThat(memberList).contains(member, member2);
+
     }
 
     @Test
     @DisplayName("회원 삭제")
     void delete() {
         //given
-        memberRepository.save(member);
+        memberRepository.save(member1);
 
         //when
-        memberRepository.deleteByEmail(member.getEmail());
+        memberRepository.deleteByEmail(member1.getEmail());
 
         //then
-        Optional<Member> findMember = memberRepository.findByEmail(member.getEmail());
+        Optional<Member> findMember = memberRepository.findByEmail(member1.getEmail());
         assertTrue(findMember.isEmpty());
     }
 }
