c32ac5dc3258375e26f1e595c58fe592395d03e1
Mandy <mandy@plgrim.com>
2021-10-05 16:16:28 +0900

    21.10.05 ErrorCode 변경, RequestMapper 메서드 분리

--- src/main/java/spring/study/Member/controller/dto/mapper/MemberRequestMapper.java
+++ src/main/java/spring/study/Member/controller/dto/mapper/MemberRequestMapper.java
@@ -1,7 +1,8 @@
 package spring.study.Member.controller.dto.mapper;
 
 
-import spring.study.Member.controller.dto.MemberRequestDTO;
+import spring.study.Member.controller.dto.MemberRequestJoinDTO;
+import spring.study.Member.controller.dto.MemberRequestModifyDTO;
 import spring.study.Member.domain.commands.MemberCommand;
 import spring.study.Member.domain.valueObjects.MemberAddressInfo;
 import spring.study.Member.domain.valueObjects.MemberBasicInfo;
@@ -9,16 +10,16 @@ import spring.study.Member.domain.valueObjects.MemberBasicInfo;
 public class MemberRequestMapper {
 
     /**
-     * dto command 변환
+     * 회원 가입 command 변환
      */
-    public MemberCommand toCommand(MemberRequestDTO dto){
+    public MemberCommand toCommand(MemberRequestJoinDTO dto){
 
         MemberBasicInfo basicVo = new MemberBasicInfo(
                 dto.getPassword(),
                 dto.getName(),
                 dto.getMobileNum(),
                 dto.getGender(),
-                dto.getBirthday()
+                dto.getBirth()
         );
 
         MemberAddressInfo addressVo = new MemberAddressInfo(
@@ -32,27 +33,28 @@ public class MemberRequestMapper {
                 .build();
     }
 
-//    /**
-//     * 회원 수정 command 변환
-//     */
-//    public MemberCommand toCommand(MemberModifyRequestDTO dto) {
-//
-//        MemberBasicInfo basicVo = new MemberBasicInfo(
-//                dto.getPassword(),
-//                dto.getName(),
-//                dto.getMobileNum(),
-//                dto.getGender(),
-//                dto.getBirthday()
-//        );
-//
-//        MemberAddressInfo addressVo = new MemberAddressInfo(
-//                dto.getAddress()
-//        );
-//
-//        return MemberCommand.builder()
-//                .email(dto.getEmail())
-//                .basicInfo(basicVo)
-//                .addressInfo(addressVo)
-//                .build();
-//    }
+    /**
+     * 회원 수정 command 변환
+     */
+    public MemberCommand toCommand(MemberRequestModifyDTO dto) {
+
+        MemberBasicInfo basicVo = new MemberBasicInfo(
+                dto.getPassword(),
+                dto.getName(),
+                dto.getMobileNum(),
+                dto.getGender(),
+                dto.getBirth()
+        );
+
+        MemberAddressInfo addressVo = new MemberAddressInfo(
+                dto.getAddress()
+        );
+
+        return MemberCommand.builder()
+                .id(dto.getId())
+                .email(dto.getEmail())
+                .basicInfo(basicVo)
+                .addressInfo(addressVo)
+                .build();
+    }
 }
