68438cffe94678ef6bc66c513e865b3a00eb7464
Mandy <mandy@plgrim.com>
2021-10-06 11:08:29 +0900

    21.10.06 MemberJpaRepository 안쓰는 메서드 주석

--- src/main/java/spring/study/Member/infraStructure/repository/MemberJPARepository.java
+++ src/main/java/spring/study/Member/infraStructure/repository/MemberJPARepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import spring.study.Member.domain.aggregates.Member;
 import spring.study.Member.domain.services.MemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberJPARepository extends MemberRepository, JpaRepository<Member, Long> {
@@ -11,9 +12,12 @@ public interface MemberJPARepository extends MemberRepository, JpaRepository<Mem
     //이메일, 전화번호 중복체크
     Optional<Member> findMemberByEmailOrMemberBasicInfo_MobileNum(String email, String mobileNum);
 
+    List<Member> findMembersByEmailOrMemberBasicInfo_MobileNum(String email, String mobileNum);
+
     //이메일 중복체크 (modify에 사용된다.)
-    Optional<Member> findByEmail(String email);
+    //Optional<Member> findByEmail(String email);
 
     //전화번호 중복체크 (modify에 사용된다.)
-    Optional<Member> findIdByMemberBasicInfo_MobileNum(String mobilNum);
+    //Optional<Member> findIdByMemberBasicInfo_MobileNum(String mobilNum);
+
 }
