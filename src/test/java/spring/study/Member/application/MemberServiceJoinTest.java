201c38f48fbba746c6ed62a3dcb2c1d37f649d10
Mandy <mandy@plgrim.com>
2021-10-07 11:09:11 +0900

    21.10.07 회원가입 ServiceTest 완료

--- src/test/java/spring/study/Member/application/MemberServiceJoinTest.java
+++ src/test/java/spring/study/Member/application/MemberServiceJoinTest.java
@@ -1,45 +1,53 @@
 package spring.study.Member.application;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import spring.study.Member.domain.aggregates.Member;
 import spring.study.Member.domain.commands.MemberCommand;
-import spring.study.Member.domain.services.MemberRepository;
 import spring.study.Member.domain.valueObjects.MemberAddressInfo;
 import spring.study.Member.domain.valueObjects.MemberBasicInfo;
+import spring.study.Member.infraStructure.repository.MemberJPARepository;
 import spring.study.common.exceptions.CustomException;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.willThrow;
 import static spring.study.common.enums.ErrorCode.DUPLICATED_MEMBER;
 
 @ExtendWith(MockitoExtension.class)
+//@MockitoSettings(strictness = Strictness.LENIENT)
+@DisplayName("[Service] 회원 가입 Test")
 class MemberServiceJoinTest {
 
     @InjectMocks
     MemberService memberService;
 
     @Mock
-    MemberRepository memberRepository;
+    MemberJPARepository memberRepository;
 
     MemberBasicInfo memberBasicInfo = MemberBasicInfo.builder()
             .password("abcd1!")
             .name("홍길동")
             .mobileNum("010-1111-1111")
             .gender("F")
-            .birthday("001122")
+            .birth("001122")
             .build();
 
     MemberAddressInfo memberAddressInfo = MemberAddressInfo.builder()
@@ -55,6 +63,7 @@ class MemberServiceJoinTest {
 
     // Member
     Member member = Member.builder()
+            .id(1L)
             .email("hong@naver.com")
             .memberBasicInfo(memberBasicInfo)
             .memberAddressInfo(memberAddressInfo)
@@ -64,25 +73,89 @@ class MemberServiceJoinTest {
     @DisplayName("회원 가입 성공")
     void joinSuccess() {
         //given
+        //빈 List
+        List<Member> testList = new ArrayList<>();
+        given(memberRepository.findByEmailOrMemberBasicInfo_MobileNum(anyString(), anyString()))
+                .willReturn(testList);
         given(memberRepository.save(any())).willReturn(member);
+
         //아래는 실패 코드 (왤까?)
         //given(memberRepository.save(member)).willReturn(member);
         //given(memberRepository.save(eq(member))).willReturn(member);
 
         //then
+        List<Member> memberList = memberRepository.findByEmailOrMemberBasicInfo_MobileNum(member.getEmail(),
+                member.getMemberBasicInfo().getMobileNum());
         Member result = memberService.join(memberCommand);
 
         //when
-        Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(member);
+        assertTrue(memberList.isEmpty());
+        assertThat(result).usingRecursiveComparison().isEqualTo(member);
     }
 
     @Test
-    @DisplayName("회원가입 실패 - 이메일 중복")
+    @DisplayName("회원 가입 실패 - 이메일 중복일 때")
     void joinFailureEmail() {
         //given
         given(memberRepository
-                .findMemberByEmailOrMemberBasicInfo_MobileNum(eq("hong@naver.com"), anyString()))
-                .willReturn(Optional.of(member))
+                .findByEmailOrMemberBasicInfo_MobileNum(eq(member.getEmail()), anyString()))
+                .willThrow(new CustomException(DUPLICATED_MEMBER));
+
+        //when
+        //then
+        assertThrows(CustomException.class, () -> memberService.join(memberCommand));
+    }
+
+    @Test
+    @DisplayName("회원 가입 실패 - 전화번호 중복일 때")
+    void joinFailureMobileNum() {
+        //given
+        given(memberRepository
+                .findByEmailOrMemberBasicInfo_MobileNum(anyString(),
+                        eq(member.getMemberBasicInfo().getMobileNum())))
+                .willThrow(new CustomException(DUPLICATED_MEMBER));
+
+        //when
+        //then
+        assertThrows(CustomException.class, () -> memberService.join(memberCommand));
+
+    }
+
+    @Test
+    @DisplayName("회원 가입 실패 - 이메일, 전화번호 모두 중복일 때")
+    void joinFailureAll() {
+        //given
+        given(memberRepository
+                .findByEmailOrMemberBasicInfo_MobileNum(
+                        eq(member.getEmail()), eq(member.getMemberBasicInfo().getMobileNum())))
+                .willThrow(new CustomException(DUPLICATED_MEMBER));
+
+        //when
+        //then
+        assertThrows(CustomException.class, () -> memberService.join(memberCommand));
+    }
+
+    /**
+     * 중복된 메서드 사용이라 @ParameterizedTest 어노테이션을 사용했다.
+     * 그런데 예외 발생 : Unexpected exception type thrown ==>
+     * expected: <spring.study.common.exceptions.CustomException>
+     * but was: <org.mockito.exceptions.misusing.PotentialStubbingProblem>
+     * given을 사용하여 동일한 메서드를 여러번 호출하면 발생 (동일한 메서드를 여러번 스텁해서 발생)
+     * 이유 : 엄격한 스텁 규칙 때문
+     * 이걸 느슨하게 해줄려면 클래스에 @MockitoSettings(strictness = Strictness.LENIENT) 붙이기
+     * 하지만 이걸 붙여주면
+     * Expected spring.study.common.exceptions.CustomException to be thrown, but nothing was thrown.
+     * 이런식으로 오류 발생
+     * 해결 실패 -> 나중에 더 찾아보기
+     * */
+    @Disabled
+    @DisplayName("회원가입 실패 - 이메일 또는 전화번호가 중복일 때")
+    @ParameterizedTest(name = "{index}: {2}")
+    @MethodSource("invalidParameters")
+    void joinFailure(String email, String mobileNum, String testName) {
+        //given
+        given(memberRepository
+                .findByEmailOrMemberBasicInfo_MobileNum(email, mobileNum))
                 .willThrow(new CustomException(DUPLICATED_MEMBER));
         //다른 방법 (성공)
         //given(memberRepository
@@ -95,18 +168,14 @@ class MemberServiceJoinTest {
 
     }
 
-    @Test
-    @DisplayName("회원가입 실패 - 전화번호 중복")
-    void joinFailureMobileNum() {
-        //given
-        given(memberRepository
-                .findMemberByEmailOrMemberBasicInfo_MobileNum(anyString(),
-                        eq("010-1111-1111")))
-                .willReturn(Optional.of(member))
-                .willThrow(new CustomException(DUPLICATED_MEMBER));
+    static Stream<Arguments> invalidParameters() {
+        final String EMAIL = "hong@naver.com";
+        final String MOBILENUM = "010-1111-1111";
 
-        //when
-        //then
-        assertThrows(CustomException.class, () -> memberService.join(memberCommand));
+        return Stream.of(
+                Arguments.of(EMAIL, "010-2222-1111", "이메일만 중복일 때"),
+                Arguments.of("hongAAA@naver.com", MOBILENUM, "전화번호만 중복일 때"),
+                Arguments.of(EMAIL, MOBILENUM, "이메일, 전화번호 모두 중복일 때")
+        );
     }
 }
