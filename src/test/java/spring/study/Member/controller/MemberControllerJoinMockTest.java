4822dbff78e5502591620d520669e70ffdd8167a
MIN-04 <kimji_0424@naver.com>
2021-10-08 15:07:54 +0900

    21.10.08 Controller 회원 가입 Test 불필요 import 삭제, MemberControllerJoinTest 삭제

--- src/test/java/spring/study/Member/controller/MemberControllerJoinMockTest.java
+++ src/test/java/spring/study/Member/controller/MemberControllerJoinMockTest.java
@@ -1,7 +1,6 @@
 package spring.study.Member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
