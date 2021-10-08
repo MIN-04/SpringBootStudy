8788f21e9c52b222333905ecd672c5da8aeadce7
MIN-04 <kimji_0424@naver.com>
2021-10-08 10:20:43 +0900

    21.10.08 MemberServiceDeleteTest 회원 탈퇴 성공 Test

--- src/test/java/spring/study/Member/application/MemberServiceDeleteTest.java
+++ src/test/java/spring/study/Member/application/MemberServiceDeleteTest.java
@@ -1,5 +1,6 @@
 package spring.study.Member.application;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,10 +12,14 @@ import spring.study.Member.domain.commands.MemberCommand;
 import spring.study.Member.domain.services.MemberRepository;
 import spring.study.Member.domain.valueObjects.MemberAddressInfo;
 import spring.study.Member.domain.valueObjects.MemberBasicInfo;
+import spring.study.Member.infraStructure.repository.MemberJPARepository;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -25,45 +30,52 @@ class MemberServiceDeleteTest {
     MemberService memberService;
 
     @Mock
-    MemberRepository memberRepository;
-
-    MemberBasicInfo memberBasicInfo = MemberBasicInfo.builder()
-            .password("abcd1!")
-            .name("홍길동")
-            .mobileNum("010-1111-1111")
-            .gender("F")
-            .birthday("001122")
-            .build();
-
-    MemberAddressInfo memberAddressInfo = MemberAddressInfo.builder()
-            .address("Seoul")
-            .build();
-
-    // MemberCommand
-    MemberCommand memberCommand = MemberCommand.builder()
-            .email("hong@naver.com")
-            .basicInfo(memberBasicInfo)
-            .addressInfo(memberAddressInfo)
-            .build();
+    MemberJPARepository memberRepository;
 
     // Member
     Member member = Member.builder()
+            .id(1L)
             .email("hong@naver.com")
-            .memberBasicInfo(memberBasicInfo)
-            .memberAddressInfo(memberAddressInfo)
+            .memberBasicInfo(MemberBasicInfo.builder()
+                    .password("abcd1!")
+                    .name("홍길동")
+                    .mobileNum("010-1111-1111")
+                    .gender("F")
+                    .birth("001122")
+                    .build())
+            .memberAddressInfo(MemberAddressInfo.builder()
+                    .address("Seoul")
+                    .build())
             .build();
 
     @Test
     @DisplayName("회원 탈퇴 성공")
     void successDelete() {
         //given
+        Long id = 1L;
+        given(memberRepository.findById(eq(id))).willReturn(Optional.of(member), Optional.empty());
+        //willDoNothing().given(memberRepository.deleteById(id));
 
         //when
-        memberService.delete(memberCommand.getEmail());
+        memberService.delete(id);
 
         //then
-        verify(memberRepository, times(1)).deleteByEmail(memberCommand.getEmail());
+        verify(memberRepository, times(1)).deleteById(id);
+        verify(memberRepository, times(2)).findById(id);
     }
 
+    @Disabled
+    @Test
+    @DisplayName("회원 탈퇴 실패 - 탈퇴할 회원이 없을 때")
+    void failDeleteNotExist() {
+
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("회원 탈퇴 실패 - 삭제가 제대로 안이루어졌을 때")
+    void failDelete() {
+
+    }
 
 }
