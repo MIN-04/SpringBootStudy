9e123a0052652f7d094ccf3afb92037f56a50d54
MIN-04 <kimji_0424@naver.com>
2021-10-18 17:07:07 +0900

    21.10.18 Product Controller 회원 목록 조회, SuccessCode 상품목록 조회 성공 추가

--- src/main/java/spring/study/Product/controller/ProductController.java
+++ src/main/java/spring/study/Product/controller/ProductController.java
@@ -2,15 +2,21 @@ package spring.study.Product.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import spring.study.Product.application.ProductService;
+import spring.study.Product.domain.aggregates.Product;
 import spring.study.common.enums.SuccessCode;
 import spring.study.common.responses.ResponseMessage;
 
+import java.util.List;
+
+import static spring.study.common.enums.SuccessCode.SUCCESS_FINDALL_PRODUCT;
 import static spring.study.common.paths.ProductUrl.PRODUCT_FIND_ALL;
 import static spring.study.common.paths.ProductUrl.PRODUCT_ROOT_PATH;
 
@@ -34,10 +40,22 @@ public class ProductController {
                 .build();
     }
 
+    /**
+     * 상품 목록 조회
+     * @param page
+     * @param pageCount
+     */
     @GetMapping(PRODUCT_FIND_ALL)
     public ResponseEntity<ResponseMessage> findAll(@RequestParam int page, @RequestParam int pageCount) {
 
-        return null;
+        log.info("Product [findAll - Controller] page = {}, pageCount = {}", page, pageCount);
+
+        Page<Product> result = productService.findAll(page, pageCount);
+        List<Product> resultList = result.getContent();
+        log.info("Product [findAll - Controller] resultList = {}", resultList);
+
+        ResponseMessage rm = setResponseMessage(SUCCESS_FINDALL_PRODUCT, resultList);
+        return ResponseEntity.ok(rm);
 
     }
 }
--- src/main/java/spring/study/common/enums/SuccessCode.java
+++ src/main/java/spring/study/common/enums/SuccessCode.java
@@ -13,7 +13,8 @@ public enum SuccessCode {
     SUCCESS_JOIN_MEMBER(OK, "회원 가입 성공"),
     SUCCESS_MODIFY_MEMBER(OK, "회원 수정 성공"),
     SUCCESS_DELETE_MEMBER(OK, "회원 삭제 성공"),
-    SUCCESS_FINDALL_MEMBER(OK, "회원 목록 조회 성공");
+    SUCCESS_FINDALL_MEMBER(OK, "회원 목록 조회 성공"),
+    SUCCESS_FINDALL_PRODUCT(OK, "상품 목록 조회 성공");
 
     private final HttpStatus httpStatus;
     private final String successMsg;
