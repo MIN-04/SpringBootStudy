e9d5b56c6807cea54fafc12618f6410ec2067e59
MIN-04 <kimji_0424@naver.com>
2021-10-26 10:01:46 +0900

    21.10.20 MemberCommand에 provider(제공사), roles(회원 권한) 추가

--- src/main/java/spring/study/Member/domain/commands/MemberCommand.java
+++ src/main/java/spring/study/Member/domain/commands/MemberCommand.java
@@ -6,11 +6,15 @@ import lombok.Getter;
 import spring.study.Member.domain.valueObjects.MemberAddressInfo;
 import spring.study.Member.domain.valueObjects.MemberBasicInfo;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @Builder
 public class MemberCommand {
     private final Long id; //id (회원 PKP
+    private final List<String> roles; //회원 권한
+    private final String provider; //제공사
     private final String email; //이메일 (회원 ID)
     private final MemberBasicInfo basicInfo;
     private final MemberAddressInfo addressInfo;
