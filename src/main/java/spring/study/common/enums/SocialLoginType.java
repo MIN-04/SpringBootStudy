97d8d8eedb63a1f612d1442de2e4901c91dfd5ad
MIN-04 <kimji_0424@naver.com>
2021-10-27 17:39:02 +0900

    21.10.27 SocialLoginType Google requestUrl 추가

--- src/main/java/spring/study/common/enums/SocialLoginType.java
+++ src/main/java/spring/study/common/enums/SocialLoginType.java
@@ -6,12 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SocialLoginType {
-    GOOGLE("google", "https://accounts.google.com/o/oauth2/v2/auth", "http://localhost:8080/login/google/callback"),
-    KAKAO("kakao", "", ""),
-    NAVER("naver", "", "");
+    GOOGLE("google", "https://accounts.google.com/o/oauth2/v2/auth", "http://localhost:8080/login/google/callback", "https://oauth2.googleapis.com/token"),
+    KAKAO("kakao", "", "", ""),
+    NAVER("naver", "", "", "");
 
     private final String socialType;
     private final String baseUrl;
     private final String redirectionUrl;
+    private final String requestUrl;
 
 }
