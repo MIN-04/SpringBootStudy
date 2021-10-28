aa3a0fbfcfac1fffcb2d9e414f9d282efc1aba8c
MIN-04 <kimji_0424@naver.com>
2021-10-28 15:17:26 +0900

    21.10.28 GOOGLE 토큰 발급에 필요한 URL 모음을 static class로 선언

---
+++ src/main/java/spring/study/common/paths/SocialOauthUrl.java
@@ -0,0 +1,11 @@
+package spring.study.common.paths;
+
+public class SocialOauthUrl {
+    /**
+     * Google
+     */
+    public static final String GOOGLE_BASE_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+    public static final String GOOGLE_REDIRECT_URL = "http://localhost:8080/login/google/callback";
+    public static final String GOOGLE_REQUEST_URL = "https://oauth2.googleapis.com/token";
+
+}
