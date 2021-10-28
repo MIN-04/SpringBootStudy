a3e8665c2a47df6c160694a4eeb1a49fb4268a70
MIN-04 <kimji_0424@naver.com>
2021-10-28 15:18:28 +0900

    21.10.28 GOOGLE 토큰 발급에 필요한 URL을 Enum에서 삭제

--- src/main/java/spring/study/common/enums/SocialLoginType.java
+++ src/main/java/spring/study/common/enums/SocialLoginType.java
@@ -6,13 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SocialLoginType {
-    GOOGLE("google", "https://accounts.google.com/o/oauth2/v2/auth", "http://localhost:8080/login/google/callback", "https://oauth2.googleapis.com/token"),
-    KAKAO("kakao", "", "", ""),
-    NAVER("naver", "", "", "");
+    GOOGLE("google"),
+    KAKAO("kakao"),
+    NAVER("naver");
 
     private final String socialType;
-    private final String baseUrl;
-    private final String redirectionUrl;
-    private final String requestUrl;
 
 }
