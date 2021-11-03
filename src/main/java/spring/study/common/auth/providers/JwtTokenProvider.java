945ab85cdabcc0fbfd79d5a4a3cab942e5140d49
MIN-04 <kimji_0424@naver.com>
2021-11-03 15:01:40 +0900

    21.11.03 JwtTokenProvider 정리

--- src/main/java/spring/study/common/auth/providers/JwtTokenProvider.java
+++ src/main/java/spring/study/common/auth/providers/JwtTokenProvider.java
@@ -54,6 +54,28 @@ public class JwtTokenProvider {
         UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
+
+    // 토큰에서 회원 정보 추출
+    public String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Request의 Header에서 token 값을 가져옵니다. "X-AUTH-TOKEN" : "TOKEN값'
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    // 토큰의 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // JWT 토큰에서 인증 정보 조회
     /*public Authentication getAuthentication(String accessToken) {
         Jws<Claims> jwsClaims = parseClaims(accessToken);
         Claims claims = jwsClaims.getBody();
@@ -82,25 +104,4 @@ public class JwtTokenProvider {
     /*public Jws<Claims> parseClaims(String accessToken) {
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(accessToken);
     }*/
-
-
-    // 토큰에서 회원 정보 추출
-    public String getUserPk(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
-    }
-
-    // Request의 Header에서 token 값을 가져옵니다. "X-AUTH-TOKEN" : "TOKEN값'
-    public String resolveToken(HttpServletRequest request) {
-        return request.getHeader("X-AUTH-TOKEN");
-    }
-
-    // 토큰의 유효성 + 만료일자 확인
-    public boolean validateToken(String jwtToken) {
-        try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
-            return !claims.getBody().getExpiration().before(new Date());
-        } catch (Exception e) {
-            return false;
-        }
-    }
 }
