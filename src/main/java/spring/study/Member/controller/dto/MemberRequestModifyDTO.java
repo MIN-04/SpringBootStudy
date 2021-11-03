9f0b83297ba2419498ed191354bc94773b786a4b
MIN-04 <kimji_0424@naver.com>
2021-11-03 11:13:48 +0900

    21.11.03 MemberRequestModifyDTO import 정리

--- src/main/java/spring/study/Member/controller/dto/MemberRequestModifyDTO.java
+++ src/main/java/spring/study/Member/controller/dto/MemberRequestModifyDTO.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import spring.study.Member.controller.validations.*;
 
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
