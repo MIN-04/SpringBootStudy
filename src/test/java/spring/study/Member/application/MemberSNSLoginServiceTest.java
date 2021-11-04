d155f97e8fd75940efabafde180586f283e22445
MIN-04 <kimji_0424@naver.com>
2021-11-04 14:04:27 +0900

    21.11.04 MemberSNSLoginService Test 로직 완료

--- src/test/java/spring/study/Member/application/MemberSNSLoginServiceTest.java
+++ src/test/java/spring/study/Member/application/MemberSNSLoginServiceTest.java
@@ -1,5 +1,6 @@
 package spring.study.Member.application;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import spring.study.Member.domain.aggregates.Member;
 import spring.study.Member.domain.services.SocialOauthService;
@@ -14,14 +16,19 @@ import spring.study.Member.infraStructure.repository.MemberJPARepository;
 import spring.study.Member.infraStructure.rest.dto.GoogleOAuthResponseDTO;
 import spring.study.Member.infraStructure.rest.dto.GoogleUserInfo;
 import spring.study.common.auth.providers.JwtTokenProvider;
+import spring.study.common.enums.ErrorCode;
+import spring.study.common.exceptions.CustomException;
 
 import java.util.Collections;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static spring.study.common.enums.ErrorCode.FAIL_LOGIN;
+import static spring.study.common.enums.ErrorCode.NOT_EXIST_MEMBER;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("[Service - login] SNS 로그인")
@@ -88,14 +95,65 @@ class MemberSNSLoginServiceTest {
     @DisplayName("SNS 회원 로그인 실패 - AccessToken 요청 실패")
     void failRequestAccessToken() {
         //given
+        ResponseEntity<GoogleOAuthResponseDTO> failResponse =
+                new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); //500error
+
+        given(socialOauthService.getAccessToken(anyString(), anyString())).willReturn(failResponse);
+
         //when
         //then
+        assertThat(failResponse.getStatusCode()).isNotEqualTo(HttpStatus.OK);
+        CustomException exception = assertThrows(CustomException.class,
+                () -> memberLoginService.loginSNS(socialLoginType, code));
+        assertThat(exception.getErrorCode()).isEqualTo(FAIL_LOGIN);
+    }
+
+    @Test
+    @DisplayName("SNS 회원 로그인 실패 - 회원 정보 요청 실패")
+    void failRequestUserInfo() {
+        //given
+        ResponseEntity<GoogleUserInfo> failResponse =
+                new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); //500error
+
+        given(socialOauthService.getAccessToken(anyString(), anyString())).willReturn(accessTokenResponse);
+        given(socialOauthService.getUserInfo(anyString(), any())).willReturn(failResponse);
+
+        //when
+        //then
+        assertThat(failResponse.getStatusCode()).isNotEqualTo(HttpStatus.OK);
+        CustomException exception = assertThrows(CustomException.class,
+                () -> memberLoginService.loginSNS(socialLoginType, code));
+        assertThat(exception.getErrorCode()).isEqualTo(FAIL_LOGIN);
+    }
+
+    @Test
+    @DisplayName("SNS 회원 로그인 실패 - 회원이 로컬 DB에 존재하지 않을 때")
+    void notExistMember() {
+        //given
+        given(socialOauthService.getAccessToken(anyString(), anyString())).willReturn(accessTokenResponse);
+        given(socialOauthService.getUserInfo(anyString(), any())).willReturn(userInfoResponse);
+        given(memberRepository.findByEmail(anyString())).willThrow(new CustomException(NOT_EXIST_MEMBER));
+
+        //when
+        //then
+        CustomException exception = assertThrows(CustomException.class,
+                () -> memberLoginService.loginSNS(socialLoginType, code));
+        assertThat(exception.getErrorCode()).isEqualTo(NOT_EXIST_MEMBER);
     }
 
     @Test
     @DisplayName("Social RedirectURL 불러오기 성공")
     void successFindSocialRedirectUrl() {
+        //given
+        String redirectUrl = "https://accounts.google.com/o/oauth2/v2/auth?scope=email";
 
+        given(socialOauthService.getRedirectUrl(anyString())).willReturn(redirectUrl);
+
+        //when
+        String resultUrl = memberLoginService.findSocialRedirectUrl(socialLoginType);
+
+        //then
+        assertThat(redirectUrl).isEqualTo(redirectUrl);
     }
 
 }
